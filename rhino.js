58e4e8283c9f21855072280d8a677019f9148707
AD7six <andydawson76@gmail.com>
2011-04-07 11:20:05 +0200

    updated rhino to run on any number of files
    added error messages too so you can find out why it's not working if
    that's the case

--- rhino.js
+++ rhino.js
@@ -10,32 +10,47 @@ Copyright (c) 2002 Douglas Crockford  (www.JSLint.com) Rhino Edition
 /*jslint rhino: true, strict: false */
 
 (function (a) {
-    var e, i, input;
-    if (!a[0]) {
-        print("Usage: jslint.js file.js");
-        quit(1);
-    }
-    input = readFile(a[0]);
-    if (!input) {
-        print("jslint: Couldn't open file '" + a[0] + "'.");
-        quit(1);
-    }
-    if (!JSLINT(input, {bitwise: true, eqeqeq: true, immed: true,
-            newcap: true, nomen: true, onevar: true, plusplus: true,
-            regexp: true, rhino: true, undef: true, white: true})) {
-        for (i = 0; i < JSLINT.errors.length; i += 1) {
-            e = JSLINT.errors[i];
-            if (e) {
-                print('Lint at line ' + e.line + ' character ' +
-                        e.character + ': ' + e.reason);
-                print((e.evidence || '').
-                        replace(/^\s*(\S*(\s+\S+)*)\s*$/, "$1"));
-                print('');
-            }
-        }
-        quit(2);
-    } else {
-        print("jslint: No problems found in " + a[0]);
-        quit();
-    }
+	var e, i, j, input, fails = 0;
+
+	if (!a.length) {
+		print("Setup problem: this function expects the first argument to be the absolute path to fulljslint.js - no arguments passed");
+		quit(1);
+	}
+	load(a[0]);
+	if (typeof(JSLINT) === undefined) {
+		print("Setup problem: this function expects the first argument to be the absolute path to fulljslint.js - the path " + a[0] + " does not contain the JSLINT object");
+		quit(1);
+	}
+	if (a.length < 2) {
+		print("Usage: jslint file.js");
+		quit(1);
+	}
+	for (i = 1; i < a.length; i += 1) {
+		input = readFile(a[i]);
+		if (!input) {
+			print("jslint: Couldn't open file '" + a[i] + "'.");
+			fails += 1;
+			continue;
+		}
+		if (!JSLINT(input, {bitwise: true, eqeqeq: true, immed: true,
+				newcap: true, nomen: true, onevar: true, plusplus: true,
+				regexp: true, rhino: true, undef: true, white: true})) {
+			print("jslint: Problems found in " + a[i]);
+			for (j = 0; j < JSLINT.errors.length; j += 1) {
+				e = JSLINT.errors[j];
+				if (e) {
+					print('Lint at line ' + e.line + ' character ' +
+							e.character + ': ' + e.reason);
+					print((e.evidence || '').replace(/^\s*(\S*(\s+\S+)*)\s*$/, "$1"));
+					print('');
+				}
+			}
+			fails += 1;
+			continue;
+		}
+		print("jslint: No problems found in " + a[i]);
+	}
+	if (fails) {
+		quit(2);
+	}
 }(arguments));
